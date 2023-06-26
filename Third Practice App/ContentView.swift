0e04676cc919e2520495be1c3903b5363b4dc8f2
Sunehri Patel <patelsunehri112gmail.com>
2023-06-26 12:37:53 -0500

    new update!

--- Third Practice App/ContentView.swift
+++ Third Practice App/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
         TextField("", text: $name)
                 .multilineTextAlignment(.center)
                 .font(.title)
-                .border(Color.gray, width:1)
+                .border(Color.purple, width:1)
                 .padding()
             Button("Submit Name") {
             textTitle = "Welcome \(name)!"
