8e0a8369281f41e36df9da06884d7648f70c697a
Sunehri Patel <patelsunehri112gmail.com>
2023-06-26 12:30:15 -0500

    Initial Commit

---
+++ Third Practice App/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  Third Practice App
+//
+//  Created by Sunehri Patel on 6/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+@State private var name = ""
+@State private var textTitle = "What is your name?"
+    var body: some View {
+        VStack {
+            Text("What is your name?")
+        TextField("", text: $name)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray, width:1)
+                .padding()
+            Button("Submit Name") {
+            textTitle = "Welcome \(name)!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Third Practice App/Third_Practice_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Third_Practice_AppApp.swift
+//  Third Practice App
+//
+//  Created by Sunehri Patel on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Third_Practice_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
